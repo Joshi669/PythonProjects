070ced4db00b4522c07dafe1c4b6a6eedb3d8789
Joshi669 <jmaduri@hotmail.com>
2023-05-17 23:50:42 -0400

    Added adjustments to snake game

--- Snake/main.py
+++ Snake/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 #Setup Screen
@@ -10,12 +12,15 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+score = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+
+
 
 game_is_on = True
 while game_is_on:
@@ -23,5 +28,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    #detect collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.score + 1
 
 screen.exitonclick()
