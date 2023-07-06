4dc7a1db1289a19bb44f86be8642e09ba23b8934
Joshi669 <jmaduri@hotmail.com>
2023-07-06 19:24:20 -0400

    Changed Snake Game

--- Snake/main.py
+++ Snake/main.py
@@ -1,25 +1,21 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
+from turtle import Screen, Turtle
 import time
+from snake import Snake
 
-#Setup Screen
+#setup Screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("Snake")
 screen.tracer(0)
 
 snake = Snake()
-food = Food()
-score = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 
 game_is_on = True
@@ -28,9 +24,5 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #detect collision
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        score.score + 1
 
 screen.exitonclick()
